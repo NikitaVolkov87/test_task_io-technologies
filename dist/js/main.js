b5a0157a59f384892d4cf4ea7c3f333dc3083796
Nikita Volkov <2545442@gmail.com>
2018-10-19 13:53:48 +0300

    html 05

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,17 @@
-function onInputChange(item) {
+function onInput(item) {
 	let el = document.getElementById('input_user-'+item);
 	console.log(el.value);
 	if ( el.value ) {
-		el.setAttribute('style', 'border-bottom: 1px solid red');
+		el.classList.add(`input-${item}-has-value`);
 	} else {
-		el.setAttribute('style', 'border-bottom: 1px solid #56627e');
+		el.classList.remove(`input-${item}-has-value`);
+	}
+}
+
+function onInputBlur(item) {
+	let inputEl = document.getElementById('input_user-'+item);
+	let errorEl = document.getElementById('error-'+item);
+	if ( !( inputEl.value.indexOf("@") != -1 && inputEl.value.indexOf(".") != -1 )) {
+		alert(item);
 	}
 }
