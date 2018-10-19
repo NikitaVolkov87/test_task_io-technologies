4648d5f2f06c9156c0c9f9422281edf2ead522bc
Nikita Volkov <2545442@gmail.com>
2018-10-19 14:29:39 +0300

    html 06

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,5 @@
 function onInput(item) {
 	let el = document.getElementById('input_user-'+item);
-	console.log(el.value);
 	if ( el.value ) {
 		el.classList.add(`input-${item}-has-value`);
 	} else {
@@ -10,8 +9,19 @@ function onInput(item) {
 
 function onInputBlur(item) {
 	let inputEl = document.getElementById('input_user-'+item);
-	let errorEl = document.getElementById('error-'+item);
-	if ( !( inputEl.value.indexOf("@") != -1 && inputEl.value.indexOf(".") != -1 )) {
-		alert(item);
+	let errorEl = document.getElementsByClassName('error-'+item)[0];
+	if ( item === 'name' ) {
+		if ( !( inputEl.value.indexOf('@') != -1 && inputEl.value.indexOf('.') != -1 )) {
+			errorEl.classList.remove('hide');
+		} else {
+			errorEl.classList.add('hide');
+		}
 	}
+}
+
+function resetPassword() {
+	let form = document.getElementsByClassName('col__login_form-item')[0];
+	form.innerHTML = `
+		<p>it's working</p>
+	`;
 }
